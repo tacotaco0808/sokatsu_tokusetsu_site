366b23d902b3a84797a2ed0614963c897c52c511
tacotaco0808 <takuto1488@gmail.com>
2025-04-06 17:20:58 +0900

    scssのグローバル変数を宣言

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import sokatsuPoster from "../assets/sokatsu_poster.png";
+import styles from "./Home.module.scss";
 export const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <img src={sokatsuPoster} alt="" />
     </div>
   );
