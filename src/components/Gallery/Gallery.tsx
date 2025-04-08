b66942e90e1341632b4fe741664e61959083dbff
tacotaco0808 <takuto1488@gmail.com>
2025-04-09 01:03:52 +0900

    gsap導入

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,14 +1,25 @@
-import { useEffect, useState } from "react";
-
+import { useGSAP } from "@gsap/react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 type GalleryProps = {
   x: number; // 0から増えていく整数
   imgs: string[];
 };
 export const Gallery = (props: GalleryProps) => {
   const [viewNum, setViewNum] = useState<number>(0); // 表示する画像の番号
-  useEffect(() => {
-    if (props.x < 0) console.error("Galleryのxは0以上で指定してください");
-    setViewNum(props.x % props.imgs.length); //余りを使って、番号を循環する
-  }, [props.x]);
-  return <img src={`images/gallery/${props.imgs[viewNum]}`} alt="" />;
+  const container = useRef<HTMLDivElement>(null); // コンテナの参照
+  const { contextSafe } = useGSAP({ scope: container }); // GSAPのコンテキストを取得
+  useEffect(
+    contextSafe(() => {
+      if (props.x < 0) console.error("Galleryのxは0以上で指定してください");
+      setViewNum(props.x % props.imgs.length); //余りを使って、番号を循環する
+      gsap.to(container.current, { rotation: "+=360" });
+    }),
+    [props.x]
+  );
+  return (
+    <div ref={container}>
+      <img src={`images/gallery/${props.imgs[viewNum]}`} alt="" />
+    </div>
+  );
 };
